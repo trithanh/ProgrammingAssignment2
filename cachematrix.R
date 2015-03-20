6c5a65754769a1fded6c27f41ec3c77cef263beb
trithanh <trithanh@users.noreply.github.com>
2015-03-20 23:06:46 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions bellow caches the inverse of a matrix
+
 
 ## Write a short comment describing this function
 
